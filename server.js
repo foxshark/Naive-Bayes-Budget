104f5508908e9eb0b3073adf07aa8c914e8e29bd
Kyle Sanders <kyle@aquaether.com>
2018-01-30 08:53:16 -0600

    very basics

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(3001, function() {
+  console.log('listening on 3001')
+})	
